27a46f33ad4aa25d06a714c498043ef537b8b976
Taomosquito <S_Peloquin123@live.com>
2024-09-10 21:47:02 -0400

    user can now favorite a photo

--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,18 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useState } from "react";
 
-import FavIcon from './FavIcon';
-import '../styles/PhotoFavButton.scss';
+import FavIcon from "./FavIcon";
+import "../styles/PhotoFavButton.scss";
 
 function PhotoFavButton() {
+  const [toggle, setToggle] = useState(false);
+  const toggleFavIcon = function () {
+    setToggle((prev) => !prev);
+  };
+
   return (
-    <div className="photo-list__fav-icon">
+    <div onClick={toggleFavIcon} className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon selected={toggle} />
       </div>
     </div>
   );
--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,29 +1,33 @@
 import React from "react";
 
 import "../styles/PhotoListItem.scss";
+import PhotoFavButton from "./PhotoFavButton";
 
 const PhotoListItem = (props) => {
   return (
-    <div className="photo-list__item">
-      <img
-        className="photo-list__image"
-        src={props.value.imageSource}
-        alt="image 1"
-      />
-      <div className="photo-list__user-details">
+    <>
+      <PhotoFavButton />
+      <div className="photo-list__item">
         <img
-          className="photo-list__user-profile"
-          src={props.value.profile}
-          alt="profile 1"
+          className="photo-list__image"
+          src={props.value.imageSource}
+          alt="image 1"
         />
-        <div className="photo-list__user-info">
-          {props.value.username}
-          <div className="photo-list__user-location">
-            {props.value.location.city}, &nbsp; {props.value.location.country}
+        <div className="photo-list__user-details">
+          <img
+            className="photo-list__user-profile"
+            src={props.value.profile}
+            alt="profile 1"
+          />
+          <div className="photo-list__user-info">
+            {props.value.username}
+            <div className="photo-list__user-location">
+              {props.value.location.city}, &nbsp; {props.value.location.country}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
 
