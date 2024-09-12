cb4a5de534ef6a551d528c267cb6dace4825aad9
Taomosquito <S_Peloquin123@live.com>
2024-09-12 02:57:24 -0400

    create navigation bar functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 
 import PhotoList from "./components/PhotoList";
-import TopicList from "components/TopicList";
+import TopNavigation from "components/TopNavigationBar";
 import "./App.scss";
 
 const App = () => {
   return (
     <div className="App">
-      {/* { Array.from(Array(3)).map((_, index) => <PhotoListItem key={index}/>) } */}
-      <TopicList />
+      <TopNavigation />
       <PhotoList />
     </div>
   );
--- frontend/src/components/TopNavigationBar.jsx
+++ frontend/src/components/TopNavigationBar.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React from "react";
+import TopicList from "./TopicList";
+import FavBadge from "./FavBadge";
 
-import '../styles/TopNavigationBar.scss'
+import "../styles/TopNavigationBar.scss";
 
 const TopNavigation = () => {
   return (
     <div className="top-nav-bar">
       <span className="top-nav-bar__logo">PhotoLabs</span>
+      <span>
+        <TopicList />
+      </span>
+      <span>
+        <FavBadge />
+      </span>
     </div>
-  )
-}
+  );
+};
 
 export default TopNavigation;
